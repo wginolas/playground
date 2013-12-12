a84a81500aef6b1a38a1a8c75a507699aaa554fd
Wolfgang Ginolas <you@example.com>
2013-12-12 09:14:13 +0100

    finished ch03 firstWord

--- haskell/rwh/ch04/firstWord.hs
+++ haskell/rwh/ch04/firstWord.hs
@@ -1,5 +1,18 @@
 #!/usr/bin/runhaskell
 
--- Print 1st word of every line.
+import Data.List (intersperse)
 
+safeHead [] = Nothing
+safeHead (x:xs) = Just x
+
+firstWord line =
+  let h = safeHead $ words line
+      w = case h of
+        Nothing -> ""
+        Just s -> s
+  in w
+
+printWords = concat . intersperse "\n" . map firstWord . lines
+
+-- Print 1st word of every line.
 main = interact printWords
