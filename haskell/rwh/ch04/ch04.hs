cb10321ae4eca43326b9566b481798a28662d567
Wolfgang Ginolas <you@example.com>
2014-01-02 11:19:10 +0100

    done something with haskell

--- haskell/rwh/ch04/ch04.hs
+++ haskell/rwh/ch04/ch04.hs
@@ -1,3 +1,4 @@
+import Data.Char (digitToInt)
 
 safeHead [] = Nothing
 safeHead (x:xs) = Just x
@@ -26,3 +27,20 @@ splitWith' p word (x:xs) =
   if p x
   then splitWith' p (x:word) xs
   else word : splitWith' p [] xs
+
+instance Monad (Either String) where
+  Right x >>= k = k x
+  Left x >>= k = Left x
+  return = Right
+  fail s = Right s
+
+saveDigitToInt d
+  | d >= '0' && d <= '9' = Right $ digitToInt d
+  | otherwise = Left $ concat ["non-digit '", [d], "'"]
+
+asPosInt = foldl f 0
+  where
+    f acc c = acc * 10 + digitToInt c
+
+asInt ('-':cs) = - asPosInt cs
+asInt cs = asPosInt cs
