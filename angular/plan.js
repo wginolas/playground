050667279d15bcc29b717243f3cb3cb890898814
Wolfgang Ginolas <wolfgang.ginolas@justsoftwareag.com>
2014-01-06 14:17:41 +0100

    Played with angular and ipython, to analyse our Redmine tickets.

---
+++ angular/plan.js
@@ -0,0 +1,11 @@
+angular.module("plan", [])
+    .controller("PlanCtrl", function () {
+        this.refresh = function() {
+            console.log("Refresh");
+        };
+    })
+    .factory("redmineRest", ["$http", function($http) {
+        var forEachTicket = function(apiKey
+        return {
+        };
+    }]);
