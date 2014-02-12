75731f53cfe27ec9cae430d75cd169e305402c91
Wolfgang Ginolas <wolfgang.ginolas@justsoftwareag.com>
2014-02-12 17:35:27 +0100

    finished ticket-graph

--- python/ticket-graph/download-tickets.py
+++ python/ticket-graph/download-tickets.py
@@ -4,10 +4,12 @@ import json
 import dateutil.parser
 import itertools
 import sqlite3
+import os.path
 
 BUG = 1
 FEATURE = 2
 SUPPORT = 3
+FEATURE_REQUEST = 4
 
 CLOSED_STATUS = {"3", "5", "6"}
 
@@ -26,14 +28,19 @@ def load_tickets(raw_issues, key):
     for i in raw_issues:
         yield load_ticket(i.id, key)
 
-    
-def download_tickets(tracker_id, fname):
+
+def create_db(fname):
     conn = sqlite3.connect(fname)
     cur = conn.cursor()
 
     cur.execute("create table tickets(id, json)");
+    cur.close()
+    return conn
     
-    with open("/home/wolfgang/keys/redmine.key") as f:
+def download_tickets(tracker_id, conn):
+    cur = conn.cursor()
+
+    with open(os.path.expanduser("~/keys/redmine.key")) as f:
         key = f.read().strip()
 
     instance = Redmine("https://redmine.justsoftwareag.com", key = key)
@@ -45,13 +52,15 @@ def download_tickets(tracker_id, fname):
         ticket_id = i["issue"]["id"]
         print ticket_id
         cur.execute("insert into tickets(id, json) values (?, ?)", (ticket_id, json.dumps(i["issue"])))
-    
-    conn.commit()
-    conn.close()
 
 
 def main():
-    download_tickets(SUPPORT, "tickets.db")
+    conn = create_db("tickets.db")
+    download_tickets(SUPPORT, conn)
+    download_tickets(BUG, conn)
+#    download_tickets(FEATURE_REQUEST, conn)
+#    download_tickets(FEATURE, conn)
+    conn.commit()
+    conn.close()
 
 main()
-
