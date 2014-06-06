8364d1451d45101ecdc31a619601e4dbedc170b2
Wolfgang Ginolas <you@example.com>
2014-06-06 11:42:29 +0200

    solved p17?

--- euler/p17.el
+++ euler/p17.el
@@ -6,6 +6,7 @@
 
 (defun p17-number-to-str (n)
   (cond
+   ((< n 1) nil)
    ((= n 1) "one")
    ((= n 2) "two")
    ((= n 3) "three")
@@ -22,7 +23,47 @@
    ((= n 15) "fifteen")
    ((= n 18) "eighteen")
    ((< n 20) (concat (p17-number-to-str (mod n 10)) "teen"))
+   ((= n 20) "twenty")
+   ((= n 30) "thirty")
+   ((= n 40) "forty")
+   ((= n 50) "fifty")
+   ((= n 80) "eighty")
+   ((and (< n 100) (= 0 (mod n 10)))
+    (concat (p17-number-to-str (/ n 10)) "ty"))
+   ((and (< n 100) (/= 0 (mod n 10)))
+    (let ((n10 (* (/ n 10) 10))
+          (n1 (mod n 10)))
+      (concat (p17-number-to-str n10) "-" (p17-number-to-str n1))))
+   ((< n 1000)
+    (let ((n100 (/ n 100))
+          (n10 (mod n 100)))
+      (concat
+       (p17-number-to-str n100)
+       " hundred"
+       (if (= 0 n10)
+           ""
+         (concat " and " (p17-number-to-str n10))))))
+   ((< n 1000000)
+    (let ((n1000 (/ n 1000))
+          (n100 (mod n 1000)))
+      (concat
+       (p17-number-to-str n1000)
+       " thousand"
+       (if (= 0 n100)
+           ""
+         (concat " " (p17-number-to-str n100))))))
    ))
 
-(p17-number-to-str 14)
-(str "1" "234")
+(with-current-buffer (get-buffer-create "numbers")
+  (erase-buffer)
+  (dotimes (i 1000)
+    (let ((n (+ i 1)))
+      (insert (p17-number-to-str n))
+      (insert "\n")))
+  (perform-replace " " "" nil nil nil))
+
+; 21124?
+
+(p17-number-to-str 123456)
+(str "1" "ninety")
+(/ 123 10)
