e58411368501f52beff0520ebc9aaafa41ba416c
Wolfgang Ginolas <you@example.com>
2014-06-18 22:02:48 +0200

    solved eulker problems

--- euler/p21.el
+++ euler/p21.el
@@ -10,13 +10,49 @@
 ;
 ; Evaluate the sum of all the amicable numbers under 10000.
 
-(defun p21-proper-divisors (n)
+(defun p21-divisor-vector-calc (len)
+  (let ((result (make-vector (+ len 1) nil))
+        (d 1)
+        (i))
+    (while (<= (* 2 d) len)
+      (setq i (* 2 d))
+      (while (<= i len)
+        (aset result i (cons d (aref result i)))
+        (setq i (+ d i)))
+      (setq d (+ 1 d)))
+    result))
+
+(setq p21-divisor-vector (p21-divisor-vector-calc 30000))
+
+(defun p21-proper-divisors-slow (n)
   (let ((result nil))
-    (dotimes (count (- (/ n 2) 1))
-      (let ((i (+ 2 count)))
+    (dotimes (count (/ n 2))
+      (let ((i (+ 1 count)))
         (when (= 0 (% n i))
           (setq result (cons i result)))))
     (reverse result)))
 
-(apply '+ (p21-proper-divisors 100))
-(= 0 (% 10 5))
+(defun p21-proper-divisors (n)
+  (aref p21-divisor-vector n))
+
+(defun p21-d (n)
+  (apply '+ (p21-proper-divisors n)))
+
+(defun p21-is-amicable (n)
+  (let ((d (p21-d n)))
+    (and (/= d n) (= n (p21-d d)))))
+
+(defun p21 ()
+  (let ((result 0))
+    (dotimes (i 9998)
+      (let ((n (+ 1 i)))
+        (when (p21-is-amicable n)
+          (setq result (+ result n)))))
+    result))
+
+;(p21)
+; 31626?
+
+;(= 0 (% 10 5))
+;(p21-is-amicable 220)
+;(p21-d 284)
--- euler/p22.el
+++ euler/p22.el
@@ -1,7 +1,28 @@
 
 
-; Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
-;
-; For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
-;
-; What is the total of all the name scores in the file?
+;;; Using names.txt (right click and 'Save Link/Target As...'), a 46K text
+;;; file containing over five-thousand first names, begin by sorting it
+;;; into alphabetical order. Then working out the alphabetical value for
+;;; each name, multiply this value by its alphabetical position in the
+;;; list to obtain a name score.
+;;;
+;;; For example, when the list is sorted into alphabetical order, COLIN,
+;;; which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the
+;;; list. So, COLIN would obtain a score of 938 × 53 = 49714.
+;;;
+;;; What is the total of all the name scores in the file?
+
+(defun p22-word-value (w)
+  (let ((result 0))
+    (dotimes (i (length w))
+      (setq result (+ result 1 (- (aref w i) ?A))))
+    result))
+
+(ert-deftest p22-word-value-test ()
+  (should (= (p22-word-value "COLIN") 53))
+  (should (= (p22-word-value "") 0))
+  (should (= (p22-word-value "A") 2)))
+
+(p22-word-value "COLIN")
+
+(- (aref  "C" 0) ?A)
