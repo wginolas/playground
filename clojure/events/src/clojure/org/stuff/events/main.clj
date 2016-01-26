9245c477c19b4fa423c6d97d85ab6924bc907767
Wolfgang <wolfgang@flunder>
2016-01-26 06:36:27 +0100

    played with clojure and android

--- clojure/events/src/clojure/org/stuff/events/main.clj
+++ clojure/events/src/clojure/org/stuff/events/main.clj
@@ -5,9 +5,14 @@
               [neko.resource :as res]
               [neko.find-view :refer [find-view]]
               [neko.threading :refer [on-ui]]
-              [neko.ui :refer [config]])
+              [neko.ui :refer [config]]
+              [clojure.string :refer [join]])
     (:import android.widget.EditText
-             android.widget.TextView))
+             android.widget.TextView
+             (java.util Calendar)
+             (android.app Activity)
+             (android.app DatePickerDialog DatePickerDialog$OnDateSetListener)
+             (android.app DialogFragment)))
 
 ;; https://github.com/alexander-yakushev/events/blob/master/tutorial.md
 
@@ -15,27 +20,7 @@
 ;; access to all application resources.
 (res/import-all)
 
-(defn notify-from-edit
-  "Finds an EditText element with ID ::user-input in the given activity. Gets
-  its contents and displays them in a toast if they aren't empty. We use
-  resources declared in res/values/strings.xml."
-  [activity]
-  (let [^EditText input (.getText (find-view activity ::user-input))]
-    (toast (if (empty? input)
-             (res/get-string R$string/input_is_empty)
-             (res/get-string R$string/your_input_fmt input))
-           :long)))
-
-(def listing (atom ""))
-
-(defn main-layout [activity]
-  [:linear-layout {:orientation :vertical}
-   [:edit-text {:hint "Event name" :id ::name}]
-   [:edit-text {:hint "Event location" :id ::location}]
-   [:button {:text "+ Event"
-             :on-click (fn [_]
-                         (add-event activity))}]
-   [:text-view {:text @listing :id ::listing}]])
+(def listing (atom (sorted-map)))
 
 (defn get-elmt [activity elmt]
   (str (.getText ^TextView (find-view activity elmt))))
@@ -43,12 +28,63 @@
 (defn set-elmt [activity elmt s]
   (on-ui (config (find-view activity elmt) :text s)))
 
+(defn format-events [events]
+  (->> (map (fn [[location event]]
+              (format "%s - %s\n" location event))
+            events)
+       (join "                      ")))
+
+(defn format-listing [lst]
+  (->> (map (fn [[date events]]
+              (format "%s - %s" date (format-events events)))
+            lst)
+       join))
+
+(defn update-ui [activity]
+  (set-elmt activity ::listing (format-listing @listing))
+  (set-elmt activity ::location "")
+  (set-elmt activity ::name ""))
+
 (defn add-event [activity]
-  (swap! listing str (get-elmt activity ::location) " - "
-         (get-elmt activity ::name) "\n")
-  (set-elmt activity ::listing @listing))
+  (let [date-key (try
+                   (read-string (get-elmt activity ::date))
+                   (catch RuntimeException e "Date string is empty!"))]
+    (when (number? date-key)
+      (swap! listing update-in [date-key] (fnil conj [])
+             [(get-elmt activity ::location) (get-elmt activity ::name)])
+      (update-ui activity))))
+
+(declare date-picker)
+(defn show-picker [^Activity activity dp]
+  (. dp show (. activity getFragmentManager) "datePicker"))
+
+(defn main-layout [activity]
+  [:linear-layout {:orientation :vertical}
+   [:edit-text {:hint "Event name" :id ::name}]
+   [:edit-text {:hint "Event location" :id ::location}]
+   [:linear-layout {:orientation :horizontal}
+    [:edit-text {:hint "Event date" :id ::date}]
+    [:button {:text "..."
+              :on-click (fn [_]
+                          (show-picker activity (date-picker activity)))}]]
+   [:button {:text "+ Event"
+             :on-click (fn [_]
+                         (add-event activity))}]
+   [:text-view {:text (format-listing @listing) :id ::listing}]])
 
 ;; (get-elmt (*a) ::name) 
+
+(defn date-picker [activity]
+  (proxy [DialogFragment DatePickerDialog$OnDateSetListener] []
+    (onCreateDialog [savedInstanceState]
+      (let [c (Calendar/getInstance)
+            year (.get c Calendar/YEAR)
+            month (.get c Calendar/MONTH)
+            day (.get c Calendar/DAY_OF_MONTH)]
+        (DatePickerDialog. activity this year month day)))
+    (onDateSet [view year month day]
+      (set-elmt activity ::date
+                (format "%d%02d%02d" year (inc month) day)))))
 
 ;; This is how an Activity is defined. We create one and specify its onCreate
 ;; method. Inside we create a user interface that consists of an edit and a
@@ -60,6 +96,7 @@
     (.superOnCreate this bundle)
     (neko.debug/keep-screen-on this)
     (on-ui
-     (set-content-view! (*a) (main-layout (*a))))
+     (set-content-view! (*a) (main-layout (*a)))
+     (set-elmt (*a) ::listing (format-listing @listing)))
 
     ))
