87f95df7012d3eafe1b719b52deee3065edfe294
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2016-07-21 09:02:02 +0200

    find jira tickets in strings

--- rust/task_browse/src/main.rs
+++ rust/task_browse/src/main.rs
@@ -1,9 +1,13 @@
 extern crate clap;
 extern crate rustc_serialize;
+extern crate regex;
+#[macro_use]
+extern crate lazy_static;
 
 use std::process::Command;
 use rustc_serialize::json;
 use clap::{Arg, App, AppSettings};
+use regex::Regex;
 
 struct Args {
     filter: Vec<String>
@@ -66,11 +70,63 @@ fn descriptions<'a>(t: &'a Task) -> Vec<&'a String> {
     result
 }
 
+fn find_just_tickets(s: &String) -> Vec<String> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"JUST-\d+").unwrap();
+    }
+
+    RE.captures_iter(s)
+        .map(|c| format!("https://jira.just.social/{}", c.at(0).unwrap()))
+        .collect()
+}
+
 fn main() {
     let args = parse_args();
 
     let tasks = load_tasks(&args.filter);
     for task in tasks {
-        println!("{:?}", task);
+        println!("{:?}", descriptions(&task));
     }
+}
+
+#[test]
+fn descriptions_extracts_description() {
+    assert_eq!(
+        vec![&"desc".to_string()],
+        descriptions(&Task {
+            id: 1,
+            description: "desc".to_string(),
+            uuid: "asdf".to_string(),
+            annotations: None}));
+}
+
+#[test]
+fn descriptions_extracts_annotations() {
+    assert_eq!(
+        vec![&"d".to_string(), &"a1".to_string(), &"a2".to_string()],
+        descriptions(&Task {
+            id: 1,
+            description: "d".to_string(),
+            uuid: "asdf".to_string(),
+            annotations: Some(vec![
+                Annotation {description: "a1".to_string()},
+                Annotation {description: "a2".to_string()}])}));
+}
+
+#[test]
+fn find_just_tickets_finds_no_ticket() {
+    assert_eq!(
+        Vec::<String>::new(),
+        find_just_tickets(&"Hallo Welt!".to_string())
+    );
+}
+
+#[test]
+fn find_just_tickets_finds_tickets() {
+    assert_eq!(
+        vec![
+            "https://jira.just.social/JUST-1".to_string(),
+            "https://jira.just.social/JUST-1234".to_string()],
+        find_just_tickets(&"Hallo JUST-1 Welt! JUST-1234".to_string())
+    );
 }
