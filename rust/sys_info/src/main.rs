75f8fb3d2ecc76f9b53ba5a1132ff02e6c68a1e9
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2016-12-01 13:09:52 +0100

    First tests

---
+++ rust/sys_info/src/main.rs
@@ -0,0 +1,106 @@
+extern crate num_cpus;
+
+use std::io;
+use std::io::{Read, BufReader, BufRead};
+use std::fs::File;
+use std::str::FromStr;
+use std::num::ParseFloatError;
+use std::thread::sleep;
+use std::time::Duration;
+use std::process::Command;
+use std::process::Stdio;
+
+#[derive(Debug)]
+enum SysInfoError {
+    Io(io::Error),
+    ParseFloat(ParseFloatError),
+    Format(String),
+}
+
+impl From<io::Error> for SysInfoError {
+    fn from(e: io::Error) -> SysInfoError {
+        SysInfoError::Io(e)
+    }
+}
+
+impl From<ParseFloatError> for SysInfoError {
+    fn from(e: ParseFloatError) -> SysInfoError {
+        SysInfoError::ParseFloat(e)
+    }
+}
+
+#[derive(Debug)]
+struct Uptime {
+    uptime: f64,
+    idle: f64,
+}
+
+struct CpuMeasureState {
+    cpus: usize,
+    last_uptime: Uptime,
+}
+
+fn uptime() -> Result<Uptime, SysInfoError> {
+    let mut f = File::open("/proc/uptime")?;
+    let mut content = String::new();
+    f.read_to_string(&mut content)?;
+    let mut iter = content.split_whitespace();
+    let uptime_str = iter.next().ok_or(SysInfoError::Format("Missing uptime".to_string()))?;
+    let idle_str = iter.next().ok_or(SysInfoError::Format("Missing idle time".to_string()))?;
+
+    Ok(Uptime {
+        uptime: f64::from_str(uptime_str)?,
+        idle: f64::from_str(idle_str)?,
+    })
+}
+
+fn init_cpu_measurement() -> Result<CpuMeasureState, SysInfoError> {
+    Ok(CpuMeasureState {
+        cpus: num_cpus::get(),
+        last_uptime: uptime()?,
+    })
+}
+
+fn cpu_usage(s: &mut CpuMeasureState) -> Result<f64, SysInfoError> {
+    let next_uptime = uptime()?;
+
+    let du = next_uptime.uptime - s.last_uptime.uptime;
+    if du < 0.001 {
+        return Ok(0.0);
+    }
+
+    let cpus = s.cpus as f64;
+    let di = (next_uptime.idle - s.last_uptime.idle);
+    println!("{} {} {:?} {:?}", di, du, s.last_uptime, next_uptime);
+
+    s.last_uptime = next_uptime;
+
+    return Ok(di / du);
+}
+
+fn main2() {
+    let mut cm = init_cpu_measurement().unwrap();
+    let delay = Duration::from_millis(10000);
+    loop {
+        sleep(delay);
+        let usage = cpu_usage(&mut cm).unwrap();
+
+        println!("{}", usage);
+    }
+}
+
+fn main() {
+    let child = Command::new("sar")
+        .arg("-r")
+        .arg("-u")
+        .arg("1")
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let input = BufReader::new(child.stdout.unwrap());
+
+    for line in input.lines().map(|l| l.unwrap()) {
+        println!("{}", line);
+    }
+}
