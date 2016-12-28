bb3a71c427f97115bfd84432022edcc8ef777007
Woflgang <wolfgang@birne>
2016-12-28 11:30:12 +0100

    working but slow

--- rust/sys_info/src/main.rs
+++ rust/sys_info/src/main.rs
@@ -1,93 +1,12 @@
-extern crate num_cpus;
+extern crate serial;
 
 use std::io;
-use std::io::{Read, BufReader, BufRead};
-use std::fs::File;
-use std::str::FromStr;
-use std::num::ParseFloatError;
-use std::thread::sleep;
-use std::time::Duration;
+use std::io::{BufReader, BufRead, Write};
 use std::process::Command;
 use std::process::Stdio;
 
-#[derive(Debug)]
-enum SysInfoError {
-    Io(io::Error),
-    ParseFloat(ParseFloatError),
-    Format(String),
-}
-
-impl From<io::Error> for SysInfoError {
-    fn from(e: io::Error) -> SysInfoError {
-        SysInfoError::Io(e)
-    }
-}
-
-impl From<ParseFloatError> for SysInfoError {
-    fn from(e: ParseFloatError) -> SysInfoError {
-        SysInfoError::ParseFloat(e)
-    }
-}
-
-#[derive(Debug)]
-struct Uptime {
-    uptime: f64,
-    idle: f64,
-}
-
-struct CpuMeasureState {
-    cpus: usize,
-    last_uptime: Uptime,
-}
-
-fn uptime() -> Result<Uptime, SysInfoError> {
-    let mut f = File::open("/proc/uptime")?;
-    let mut content = String::new();
-    f.read_to_string(&mut content)?;
-    let mut iter = content.split_whitespace();
-    let uptime_str = iter.next().ok_or(SysInfoError::Format("Missing uptime".to_string()))?;
-    let idle_str = iter.next().ok_or(SysInfoError::Format("Missing idle time".to_string()))?;
-
-    Ok(Uptime {
-        uptime: f64::from_str(uptime_str)?,
-        idle: f64::from_str(idle_str)?,
-    })
-}
-
-fn init_cpu_measurement() -> Result<CpuMeasureState, SysInfoError> {
-    Ok(CpuMeasureState {
-        cpus: num_cpus::get(),
-        last_uptime: uptime()?,
-    })
-}
-
-fn cpu_usage(s: &mut CpuMeasureState) -> Result<f64, SysInfoError> {
-    let next_uptime = uptime()?;
-
-    let du = next_uptime.uptime - s.last_uptime.uptime;
-    if du < 0.001 {
-        return Ok(0.0);
-    }
-
-    let cpus = s.cpus as f64;
-    let di = (next_uptime.idle - s.last_uptime.idle);
-    println!("{} {} {:?} {:?}", di, du, s.last_uptime, next_uptime);
-
-    s.last_uptime = next_uptime;
-
-    return Ok(di / du);
-}
-
-fn main2() {
-    let mut cm = init_cpu_measurement().unwrap();
-    let delay = Duration::from_millis(10000);
-    loop {
-        sleep(delay);
-        let usage = cpu_usage(&mut cm).unwrap();
-
-        println!("{}", usage);
-    }
-}
+use serial::prelude::*;
+use serial::SerialDevice;
 
 fn open_sar_stream() -> io::Result<Box<BufRead>> {
     let child = Command::new("sar").arg("-r")
@@ -99,22 +18,59 @@ fn open_sar_stream() -> io::Result<Box<BufRead>> {
     Ok(Box::new(BufReader::new(child.stdout.unwrap())))
 }
 
+fn parse_and_scale(s: &str, old_min: f64, old_max: f64, new_min: f64, new_max: f64) -> u8 {
+    let mut f = s.replace(",", ".").parse::<f64>().unwrap_or(0.0);
+    f = (f - old_min) * (new_max - new_min) / (old_max - old_min) + new_min;
+    f = f.min(new_max).max(new_min);
+
+    f as u8
+}
+
+fn send_info(port: &mut serial::SystemPort, idle_str: &str, mem_used_str: &str) -> serial::Result<()> {
+    let cpu_used = parse_and_scale(idle_str, 100.0, 0.0, 0.0, 235.0);
+    let mem_used = parse_and_scale(mem_used_str, 0.0, 100.0, 0.0, 235.0);
+    println!("idle_str: {}  mem_used_str: {}  cpu used: {}  mem used: {}",
+             idle_str,
+             mem_used_str,
+             cpu_used,
+             mem_used);
+
+    port.write_all(&[33, cpu_used, mem_used])?;
+    port.flush()?;
+
+    Ok(())
+}
+
+fn open_serial_port() -> serial::Result<serial::SystemPort> {
+    let mut port = serial::open("/dev/ttyUSB0")?;
+    let mut settings = port.read_settings()?;
+    settings.set_baud_rate(serial::Baud9600)?;
+    settings.set_parity(serial::ParityNone);
+    settings.set_stop_bits(serial::Stop1);
+    port.write_settings(&settings)?;
+
+    Ok(port)
+}
+
 fn main() {
     let input = open_sar_stream().unwrap();
+    let mut port = open_serial_port().unwrap();
+    let mut idle = String::new();
+    let mut mem_used = String::new();
 
     let mut last_line = String::new();
     for line in input.lines().map(|l| l.unwrap()) {
 
         if !line.is_empty() && !last_line.is_empty() {
-
             for (k, v) in last_line.split_whitespace().zip(line.split_whitespace()) {
                 if k == "%idle" {
-                    println!("idle: {}", v);
+                    idle = v.to_string();
                 }
                 if k == "%commit" {
-                    println!("mem used: {}", v);
+                    mem_used = v.to_string();
                 }
             }
+            send_info(&mut port, &idle, &mem_used).unwrap();
         }
 
         last_line = line;
